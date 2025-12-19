88b09fe2837168f85c512f4218d8e948d71f8d66
Gorkhaleaf <gorkhaleaf@gmail.com>
2025-12-19 12:52:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year cache for optimized images
-    domains: ['cdn-icons-png.flaticon.com', 'localhost'],
+    domains: ['cdn-icons-png.flaticon.com', 'localhost', 'ioguexibabxzfbmmnwzg.supabase.co'],
     remotePatterns: [
       {
         protocol: 'https',
