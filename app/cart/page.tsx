9be993d9d3b1f6c88f8f243c8f609928abc9f046
Gorkhaleaf <gorkhaleaf@gmail.com>
2025-12-19 16:13:42 +0530

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
 
-// Load the cart client component ONLY in the browser
 const CartPageClient = dynamic(() => import("./CartPageClient"), {
   ssr: false,
 });
