7b24a5c642703a803b492b388f3c96aad01db462
Gorkhaleaf <gorkhaleaf@gmail.com>
2025-12-19 14:31:37 +0530

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+export const dynamic = "force-dynamic";
 import { useState, useEffect, useCallback } from "react"
 import { Header } from "@/components/Header"
 import Footer from "@/components/Footer"
