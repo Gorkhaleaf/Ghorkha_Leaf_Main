c199349c17de1ebff0d447f128c968745a108cbc
Gorkhaleaf <gorkhaleaf@gmail.com>
2026-01-11 10:38:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,31 @@ export default function RootLayout({
       className={`${inter.variable} ${playfair.variable}`}
     >
       <body className={inter.className}>
+        {/* Meta Pixel */}
+<Script id="meta-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '713605271613928');
+    fbq('track', 'PageView');
+  `}
+</Script>
+
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=713605271613928&ev=PageView&noscript=1"
+  />
+</noscript>
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-839SCRG86M"
