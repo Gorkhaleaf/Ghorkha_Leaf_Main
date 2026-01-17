1bc2dfa6553758c57c142f8f5b0c01761f595d56
Gorkhaleaf <gorkhaleaf@gmail.com>
2026-01-17 15:35:46 +0530

    Create ProductViewTracker.tsx

---
+++ components/meta/ProductViewTracker.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import ViewContentPixel from "@/components/meta/ViewContentPixel";
+
+export default function ProductViewTracker({ product }: { product: any }) {
+  return <ViewContentPixel product={product} />;
+}
